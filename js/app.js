58bbf979d9d9f28778caa33d141232c71c6cb199
Sacristan2017 <31742625+Sacristan2017@users.noreply.github.com>
2026-05-18 09:06:52 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Mensaje dinámico en el header
+document.getElementById("mensaje").textContent = "Calidad tradicional desde 1950";
+
+// Acción del botón
+document.getElementById("boton").addEventListener("click", function() {
+    document.getElementById("resultado").textContent = "Gracias por visitar nuestra página";
+});
